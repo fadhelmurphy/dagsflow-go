bde8e37de3243a7ae4557b6a2c6b0894e074c436
Fadhel EDTS <fadhel.ijlal@sg-edts.com>
2025-06-23 16:25:51 +0700

    chore: update example dag

--- dags/dag1.go
+++ dags/dag1.go
@@ -9,10 +9,15 @@ func init() {
 	d := dag.NewDAG("dag1", "*/1 * * * *")
 	a := d.NewJob("a", func(ctx *dag.Context) {
 		ctx.DAG.Logf("[DAG1] Run A PAKE LOGF")
-		fmt.Println("[DAG1] Run A")
+		ctx.DAG.Logf("[DAG1] Run A")
+		ctx.SetXCom("sebuah-key", "sebuah-nilai")
+
 	})
 	b := d.NewJob("b", func(ctx *dag.Context) {
-		fmt.Println("[DAG1] Run B")
+		getKey := ctx.GetXCom("sebuah-key")
+		logGetKey := fmt.Sprintf("%s << ini nilai dari key", getKey)
+		ctx.DAG.Logf(logGetKey)
+		ctx.DAG.Logf("[DAG1] Run B")
 	})
 	a.Then(b)
 	dag.Register(d)
--- dags/dag2.go
+++ dags/dag2.go
@@ -8,7 +8,8 @@ import (
 func init() {
 	d := dag.NewDAG("dag2", "*/2 * * * *")
 	x := d.NewJob("x", func(ctx *dag.Context) {
-		fmt.Println("[DAG2] Run X")
+		fmt.Println("[DAG2] Run 2")
+		ctx.DAG.Logf("[DAG2] Run 2")
 		ctx.SetXCom("key", "value")
 	})
 	y := d.NewJob("y", func(ctx *dag.Context) {
