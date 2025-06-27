5b7d4bcc25e80ef5a5ab1cf5d8757123292b5692
PC Fadhel Ngepush! <krazefollow@gmail.com>
2025-06-27 22:22:47 +0700

    feat: add trigger rule

--- dags/trigger_rule.go
+++ dags/trigger_rule.go
@@ -7,27 +7,22 @@ import (
 func init() {
 	d := dag.NewDAG("trigger_rule_dag", "@every 1s")
 
-	// Job A - selalu sukses
 	jobA := d.NewJob("job_a", func(ctx *dag.Context) {
 		ctx.DAG.Logf("Job A running (success)")
 	})
 
-	// Job B - gagal (panic)
 	jobB := d.NewJob("job_b", func(ctx *dag.Context) {
 		panic("Simulasi error di Job B")
 	})
 
-	// Job C - jalan jika semua upstream (A dan B) sukses
 	jobC := d.NewJob("job_c_all_success", func(ctx *dag.Context) {
 		ctx.DAG.Logf("Job C jalan karena semua upstream sukses")
 	}).WithTriggerRule(dag.AllSuccess)
 
-	// Job D - jalan jika semua upstream (A dan B) gagal
 	jobD := d.NewJob("job_d_all_failed", func(ctx *dag.Context) {
 		ctx.DAG.Logf("Job D jalan karena semua upstream gagal")
 	}).WithTriggerRule(dag.AllFailed)
 
-	// Job E - jalan walaupun ada yang gagal
 	jobE := d.NewJob("job_e_always", func(ctx *dag.Context) {
 		ctx.DAG.Logf("Job E jalan karena trigger rule = always")
 	}).WithTriggerRule(dag.Always)
